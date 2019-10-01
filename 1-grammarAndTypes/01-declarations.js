96f53f2b2adcdbd11377b290015594dc0a2b9aca
DoIUseMyRealName <connorwolfford@gmail.com>
2019-10-01 09:44:16 -0400

    message

---
+++ 1-grammarAndTypes/01-declarations.js
@@ -0,0 +1,17 @@
+let a = 2;
+
+let x;
+console.log('Declaration:', x);
+
+x=10;
+console.log('Initializing 1:', x);
+
+let y = 'Hello';
+console.log('Both:', x, y)
+
+let today = 'great!';
+const elevenFifty = 'Wonderful';
+console.log(today, elevenFifty)
+
+today = 'Lovely';
+console.log(today, elevenFifty)
---
+++ 1-grammarAndTypes/02-Types.js
@@ -0,0 +1,34 @@
+//Boolean be like yo fool im true or false son
+
+let i = true;
+let j = false;
+
+console.log(i);
+
+//Null is an empty value
+
+let doxMyself = {
+    firstName: 'Connor',
+    lastName: 'Wolfford',
+    houseNumber: 15739,
+    Street: 'Tundra Pointe',
+    City: 'Noblseville',
+    State: 'Indiana',
+    zipcode: 46060,
+}
+
+console.log(doxMyself)
+
+let firstName = 'Connor ';
+let lastName = 'Wolfford ';
+let houseNumber = 15739;
+let street = 'Tundra Pointe ';
+let city = 'Noblesville ';
+let state = 'Indiana ';
+let zipcode = 46060;
+
+console.log(firstName + lastName + houseNumber + ' ' + street + city + state + zipcode)
+
+let sent = 'This sentence will be split into individual parts';
+
+console.log(sent.split(' '));
