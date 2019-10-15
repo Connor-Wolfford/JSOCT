d5ff674054d970566178a19c2ba22e7f81168bd7
Connor-Wolfford <bigaleinstein2000@gmail.com>
2019-10-15 16:07:40 -0400

    deployment

--- Projects/API/index.js
+++ Projects/API/index.js
@@ -1,15 +1,18 @@
 const baseURL = 'https://rickandmortyapi.com/api/character/'
 
 const portalBtn = document.querySelector('.clickPlease')
-// const portal = new Image();
-// portal.src = 
 const div = document.querySelector('portal')
+const section = document.querySelector('section')
+
+// document.getElementsByClassName("clickPlease").onclick = function() {
+// document.getElementById("theImage").style.visibility = "visible";
+// }
 
 portalBtn.addEventListener('click', portalActivate)
 
 function portalActivate () {
     console.log('CLICK')
-
+    document.getElementById("theImage").style.visibility = "visible";
 fetch(baseURL)
     .then(function(result) {
         // console.log(result)
@@ -22,11 +25,11 @@ fetch(baseURL)
 
 function character (json) {
     // console.log(json.results[0]);
-    
     let name = document.createElement('h2')
     let para = document.createElement('p')
     let img = document.createElement('img')
     let article = document.createElement('article')
+    let clearfix = document.createElement('div')
 
     let chars = json.results[0]
 
@@ -35,11 +38,11 @@ function character (json) {
     img.innerHTML = chars.image
 
 
+
     console.log('Character:', chars)
 
     article.appendChild(name)
     article.appendChild(img)
     article.appendChild(para)
-    // div.appendChild(article)
-
+    section.appendChild(article)
 }
